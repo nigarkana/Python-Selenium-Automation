85419d743d4aeea036c90c06aacbd153cef32e3d
Nigar Sultana Kana <nigarkana@gmail.com>
2025-04-06 12:17:46 -0700

    Locator find out

--- locators.py
+++ locators.py
@@ -1,7 +1,22 @@
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://rahulshettyacademy.com/angularpractice/")
-driver.find_element(By.NAME, 'name')
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Mahid")
+driver.find_element(By.NAME, 'email').send_keys("hello@gmail.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys(("Test@job1"))
+driver.find_element(By.CLASS_NAME, "form-check-input").click()
+#driver.find_element(By.ID, "exampleFormControlSelect1").send_keys("Male").click()
+#Xpath locator = //tagname[@attribute='value']
+#CSS Selector = tagname[attribute ='value']
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
+message = driver.find_element(By.CLASS_NAME, "alert-success").text
+print(message)
+assert "success" in message
+
+
+time.sleep(2)
